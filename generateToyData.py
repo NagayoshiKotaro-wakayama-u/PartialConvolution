17416fe22d77597ca4b48485f52fb7ec27c92733
NagayoshiKotaro-wakayama-u <nagayoshi.kotaro@g.wakayama-u.jp>
2020-12-10 14:05:58 +0900

    update

--- generateToyData.py
+++ generateToyData.py
@@ -2,9 +2,11 @@ import numpy as np
 import cv2
 import os
 import sys
+import re
 import random
+import pdb
 from argparse import ArgumentParser
-
+from PIL import Image
 
 #二次元正規分布の確率密度を返す関数
 def gaussian(x,sigma,mu):
@@ -18,35 +20,114 @@ def gaussian(x,sigma,mu):
     return np.exp(-diag/2.0) / (np.sqrt((2 * np.pi) ** n * det))
 
 def parse_args():
-    parser = ArgumentParser(description='Training script for PConv inpainting')
+    parser = ArgumentParser(description='Generate toy data for Inpainting')
+    parser.add_argument('-loadMask','--loadMaskPath',default="",help='using exist Mask Image, you have to set the path. (default="" (generate new mask))')
     parser.add_argument('-train', '--train',type=int, default=1500,help='number of training images (default=1500)')
     parser.add_argument('-valid', '--valid',type=int, default=100,help='number of validation images (default=100)')
     parser.add_argument('-test', '--test',type=int, default=100,help='number of test images (default=100)')
     parser.add_argument('-masktype', '--masktype',type=str, default='same', help='generate same mask images or vary mask images (default=same)', choices=['same', 'vary'])
     parser.add_argument('-ratio','--maskratio',type=float,default=0.99,help='ratio of mask (default=0.99)')
-    parser.add_argument('-dspath','--dataSetPath',type=str,default="gaussianToyData",help='Path of dataSet')
+    parser.add_argument('-dspath','--dataSetPath',type=str,default="gaussianToyData",help='Name of dataSet (default=gaussianToyData)')
     parser.add_argument('-posVariant','--positionVariant',action='store_true',help="Flag for making data position variant")
+    parser.add_argument('-mixed','--isMixedGaussian',action='store_true',help="Flag for making data mixed-gaussian (if you use this, -posVariant is not available)")
 
     return  parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
+    if args.loadMaskPath!="":
+        ext = args.loadMaskPath.split(".")[-1]
+        if ext == "png" or ext == "jpg":
+            existMask = np.array(Image.open(args.loadMaskPath))/255
+        elif ext == "pickle" or ext == "pkl":
+            existMask = pickle.load(open(args.loadMaskPath,"rb"))
+
+    # 混合分布と位置依存は同時に使えない
+    if args.isMixedGaussian and args.positionVariant:
+        print("you can't use option -mixed and -posVariant at the same time")
+        sys.exit()
 
     dsPath = "data" + os.sep + args.dataSetPath # データセットのパス
+
     shape = (512,512) # 画像のサイズ
-    dataNum = {"train":args.train,"valid":args.valid,"test":args.test} # train,validation,testでの生成数
- 
-    #================================================================================================
-    # 画像の作成
-    pointNum = [1,1] # ガウス分布の数 ([2,3]なら2~3個のガウス分布による混合ガウスになる)
     keys = ["train","valid","test"]
+    dataNum = {keys[0]:args.train,keys[1]:args.valid,keys[2]:args.test} # train,validation,testでの生成数
     dataPath = [dsPath+os.sep+k+os.sep+k+"_img" for k in keys]
 
     for DIR in dataPath:
         if not os.path.isdir(DIR):
+            os.makedirs(DIR)     
+
+    if args.isMixedGaussian:
+        pointNum = [2,3] # ガウス分布の数 ([2,3]なら2~3個のガウス分布による混合ガウスになる)
+    else:
+        pointNum = [1,1]
+
+    # imagePath = [dsPath + os.sep + k + ".pickle" for k in keys]
+
+    #================================================================================================
+    # マスク画像の作成
+    maskPath = [dsPath+os.sep+k+"_mask" for k in keys]
+    # maskPath = [dsPath + os.sep+k+"_mask.pickle" for k in keys]
+    maskRatio = args.maskratio # マスク部分の割合
+    masktype = args.masktype
+
+    for DIR in maskPath: # マスクのディレクトリを作成
+        if not os.path.isdir(DIR):
             os.makedirs(DIR)
 
+    def random_point_nodup(num): # 重複なしで座標を生成
+        res = []
+        while len(res) < num:
+            x = random.randint(0,shape[0]-1)
+            y = random.randint(0,shape[1]-1)
+            dupflag = False # 重複フラグ
+            for p in res:
+                if p[0]==x and p[1]==y:
+                    dupflag = True
+                    break
+            if not dupflag:
+                res.append([x,y])
+
+        return res
+ 
+    obsNum = int(shape[0]*shape[1]*(1-maskRatio))
+    if args.loadMaskPath == "":
+        if masktype == "same":
+            mask = np.zeros(shape)
+            # 観測点を生成し,マスクの要素を1 (画像として保存するので255) にする
+            obs = random_point_nodup(obsNum)
+            for p in obs:
+                mask[p[0],p[1]] = 255
+            mask = mask.astype("uint8")
+
+    for path,key in zip(maskPath,keys):
+        # masks = []
+        print("generate "+key+"_mask data")
+        for i in range(dataNum[key]):
+            print("\r progress:{0}/{1}".format(i+1,dataNum[key]),end="")
+            if masktype=="same":
+                if args.loadMaskPath !="":
+                    pass
+                    # masks.append(existMask)
+                else:
+                    pass
+                    # masks.append(mask)    
+                cv2.imwrite(path+os.sep+"{0:04}.png".format(i),mask)
+            else:
+                mask = np.zeros(shape)
+                obs = random_point_nodup(obsNum)
+                for p in obs:
+                    mask[p[0],p[1]] = 255
+                # masks.append(mask)
+                mask = mask.astype("uint8")
+                cv2.imwrite(path+os.sep+"{0:04}.png".format(i),mask)
+        # pickle.dump(np.array(masks),open(path,"wb"))
+        print("")
+
+    #=============================================================
+    # 画像の作成
     # XY座標を生成
     x = np.arange(0, shape[0], 1)
     y = np.arange(0, shape[1], 1)
@@ -80,19 +161,21 @@ if __name__ == "__main__":
                 xx = 4000
                 yy = 4000
                 label = 4
-            else:
-                randRange = [2000,10000]
-                dis = randRange[1]-randRange[0]
-                xx = random.random()*dis+randRange[0]
-                yy = random.random()*dis+randRange[0]
-                xy = (random.random()*2-1)*randRange[0]
-                label = 0
+        else:
+            randRange = [2000,10000]
+            dis = randRange[1]-randRange[0]
+            xx = random.random()*dis+randRange[0]
+            yy = random.random()*dis+randRange[0]
+            xy = (random.random()*2-1)*randRange[0]
+            label = 0
 
         sigma = np.array([[xx,xy],[xy,yy]])
         return mu,sigma,label
 
     # データの生成
     for path,key in zip(dataPath,keys):
+    # for path,key in zip(imagePath,keys):
+        imgs = []
         print("generate "+key+" data")
         for i in range(dataNum[key]):
             print("\r progress:{0}/{1}".format(i+1,dataNum[key]),end="")
@@ -109,53 +192,8 @@ if __name__ == "__main__":
             weight = random.random()*128+127
             img = (Z/np.max(Z))*weight
             cv2.imwrite(path+os.sep+"{0:04}_lab{1}.png".format(i,labels),img.astype("uint8"))
+            # imgs.append(Z/max(Z))
+        # pickle.dump(np.array(imgs),open(path,"wb"))
         print("")
 
-    #================================================================================================
-    # マスク画像の作成
-    maskPath = [dsPath+os.sep+k+"_mask" for k in keys]
-    maskRatio = args.maskratio # マスク部分の割合
-    masktype = args.masktype
 
-    for DIR in maskPath: # マスクのディレクトリを作成
-        if not os.path.isdir(DIR):
-            os.makedirs(DIR)
-
-    def random_point_nodup(num): # 重複なしで座標を生成
-        res = []
-        while len(res) < num:
-            x = random.randint(0,shape[0]-1)
-            y = random.randint(0,shape[1]-1)
-            dupflag = False
-            for p in res:
-                if p[0]==x and p[1]==y:
-                    dupflag = True
-                    break
-            if not dupflag:
-                res.append([x,y])
-
-        return res
- 
-    obsNum = int(shape[0]*shape[1]*(1-maskRatio))
-    if masktype == "same":
-        mask = np.zeros(shape)
-        # 観測点を生成し,マスクの要素を1 (画像として保存するので255) にする
-        obs = random_point_nodup(obsNum)
-        for p in obs:
-            mask[p[0],p[1]] = 255
-        mask = mask.astype("uint8")
-
-    for path,key in zip(maskPath,keys):
-        print("generate "+key+"_mask data")
-        for i in range(dataNum[key]):
-            print("\r progress:{0}/{1}".format(i+1,dataNum[key]),end="")
-            if masktype=="same":
-                cv2.imwrite(path+os.sep+"{0:04}.png".format(i),mask)
-            else:
-                mask = np.zeros(shape)
-                obs = random_point_nodup(obsNum)
-                for p in obs:
-                    mask[p[0],p[1]] = 255
-                mask = mask.astype("uint8")
-                cv2.imwrite(path+os.sep+"{0:04}.png".format(i),mask)
-        print("")
