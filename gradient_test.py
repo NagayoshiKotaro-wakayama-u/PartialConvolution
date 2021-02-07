376d9c5c29ab24d653812d5c743f77098e9b72d2
Hirotaka Hachiya <hhachiya@wakayama-u.ac.jp>
2021-02-07 22:48:59 +0900

    add grad2 to KLgradient_test.ipynb

--- gradient_test.py
+++ gradient_test.py
@@ -1,20 +1,24 @@
+import pdb
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
+u_gt = 0.0
+alpha = 10
+
 sess = tf.Session()
 
-x = tf.Variable([0.0,0.5,1.5,1.1,2.0,0.3,0.1])
+x = tf.Variable([1.1,1.5,2.0,0.5,1.1])
+x_ind = tf.Variable([0.0,1.0,2.0,3.0,4.0])
 thre = 1.0
 
-xthre = tf.where(x>thre, x, tf.zeros_like(x))
-output_where = tf.square(xthre)
-gradient_where = tf.gradients(output_where, x)[0]
+#x_sig = 1/(1+tf.exp(-alpha*(x-thre)))
+x_ind_thre = x*x_ind
+u_pred = tf.reduce_mean(x_ind_thre)
 
-x_mask = tf.greater(x,1.0)
-output_mask = tf.square(tf.boolean_mask(x,x_mask))
-gradient_mask = tf.gradients(output_mask,x)[0]
+output = tf.square(u_gt - u_pred)
 
+grad_x = tf.gradients(output, x)[0]
+grad_x_ind = tf.gradients(output, x_ind)[0]
 
 sess.run(tf.global_variables_initializer())
-print(sess.run([x, output_where, gradient_where])) 
-print(sess.run([x, output_mask, gradient_mask])) 
+print(sess.run([x, x_ind_thre, u_pred, output, grad_x, grad_x_ind])) 
